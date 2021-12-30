526d3a7916ad2cee5b59f64a638dc8a45ea99e6c
Maria Magana <maria.imagana15@gmail.com>
2021-12-29 20:29:14 -0800

    updating version

--- app/routes.py
+++ app/routes.py
@@ -1,2 +1,16 @@
 from flask import Blueprint
 
+hello_world_bp = Blueprint("hello_world", __name__)
+
+@blueprint_name.route("/endpoint/path/here", methods=["GET"])
+def endpoint_name():
+    my_beautiful_response_body = "Hello, World!"
+    return my_beautiful_response_body
+
+@hello_world_bp.route("/hello/JSON", methods=["GET"])
+def say_hello_json():
+    return {
+        "name": "Ada Lovelace",
+        "message": "Hello!",
+        "hobbies": ["Fishing", "Swimming", "Watching Reality Shows"]
+    }
